e96b9105c028feb5aa2dbf27061bc5d80faadb52
RCssZero <sranchvook@outlook.com>
2022-10-05 22:04:47 +0800

    update

--- FGSM/main.py
+++ FGSM/main.py
@@ -7,7 +7,6 @@ from torchvision import datasets, transforms
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 epsilons = [0, .05, .1, .15, .2, .25, .3]
 pretrained_model = "data/lenet_mnist_model.pth"
 use_cuda=True
@@ -30,21 +29,6 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, download=True, transform=transforms.Compose([
-            transforms.ToTensor(),
-            ])),
-        batch_size=1, shuffle=True)
-
-print("CUDA Available: ",torch.cuda.is_available())
-device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
-
-model = Net().to(device)
-
-model.load_state_dict(torch.load(pretrained_model, map_location='cpu'))
-
-model.eval()
-
 def fgsm_attack(image, epsilon, data_grad):
     sign_data_grad = data_grad.sign()
     perturbed_image = image + epsilon*sign_data_grad
@@ -57,30 +41,22 @@ def test( model, device, test_loader, epsilon ):
     adv_examples = []
 
     for data, target in test_loader:
-
         data, target = data.to(device), target.to(device)
-
         data.requires_grad = True
-
         output = model(data)
         init_pred = output.max(1, keepdim=True)[1] 
-
+        
         if init_pred.item() != target.item():
             continue
-
+        
         loss = F.nll_loss(output, target)
-
         model.zero_grad()
-
         loss.backward()
-
         data_grad = data.grad.data
-
         perturbed_data = fgsm_attack(data, epsilon, data_grad)
-
         output = model(perturbed_data)
-
         final_pred = output.max(1, keepdim=True)[1] 
+        
         if final_pred.item() == target.item():
             correct += 1
             if (epsilon == 0) and (len(adv_examples) < 5):
@@ -95,6 +71,20 @@ def test( model, device, test_loader, epsilon ):
     print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
 
     return final_acc, adv_examples
+
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=False, download=True, transform=transforms.Compose([
+            transforms.ToTensor(),
+            ])),
+        batch_size=1, shuffle=True)
+
+print("CUDA Available: ",torch.cuda.is_available())
+device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
+
+model = Net().to(device)
+model.load_state_dict(torch.load(pretrained_model, map_location='cpu'))
+model.eval()
 
 accuracies = []
 examples = []
